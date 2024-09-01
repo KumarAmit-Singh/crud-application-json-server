3ac0d6b1b1eb0cc50df1067877df6cb949603342
Amit <singhaks1527@gmail.com>
2024-09-01 14:12:55 +0530

    add features

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import EmpListing from './EmpListing';
-import EmpCreate from './EmpCreate';
+// import EmpCreate from './EmpCreate';
+import EmpCreate from './EmpCreate'
 import EmpDetail from './EmpDetail';
 import EmpEdit from './EmpEdit';
 
@@ -16,6 +17,7 @@ function App() {
         <Routes>
           
           <Route path='/' element={<EmpListing />}></Route>
+          {/* <Route path='/employee/create' element={<EmpCreate />}></Route> */}
           <Route path='/employee/create' element={<EmpCreate />}></Route>
 
           <Route path='/employee/detail/:empid' element={<EmpDetail />}></Route>
